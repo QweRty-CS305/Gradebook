4fe32dceb5100443560b565bfd77ea8f4dcd8c04
Brian Bacon <bacon011@connect.wcsu.edu>
2018-11-08 09:57:44 -0500

    Created file path for qwerty related things

---
+++ qwerty/src/db/addSeasonMgmt.sql
@@ -0,0 +1,93 @@
+-- addSeasonMgmt.sql
+
+-- Brian Bacon, Jake Homberg
+-- Team Qwerty
+
+-- Script to add functions related to season management
+
+\o spoolTestData.txt
+
+\qecho -n 'Script run on '
+\qecho -n `date /t`
+\qecho -n 'at '
+\qecho -n `time /t`
+\qecho -n ' by '
+\qecho :USER
+\qecho ' '
+
+--Suppress messages below WARNING level for the duration of this script
+SET LOCAL client_min_messages TO WARNING;
+
+
+--Function to get the details of the season matching a "season identification"
+-- a season identification is season order, season code, or season name
+-- performs case-insensitive match of season name and code
+-- this function makes it easier for users to indicate a season by any of the
+-- three possible identifiers for seasons
+DROP FUNCTION IF EXISTS qwerty.getSeason(VARCHAR(20));
+
+CREATE FUNCTION qwerty.getSeason(seasonIdentification VARCHAR(20))
+RETURNS TABLE
+(
+   "Order" NUMERIC(1,0),
+   Name VARCHAR(20),
+   Code CHAR(1)
+)
+AS
+$$
+
+   SELECT "Order", Name, Code
+   FROM qwerty.Season
+   WHERE CASE
+            WHEN $1 ~ '^[0-9]$' THEN "Order" = to_number($1,'9')
+            WHEN LENGTH($1) = 1 THEN Code = UPPER($1)
+            ELSE LOWER(TRIM(Name)) = LOWER(TRIM($1))
+         END;
+
+$$ LANGUAGE sql
+   STABLE
+   RETURNS NULL ON NULL INPUT
+   ROWS 1;
+
+
+--Function to get the details of the season matching a season order
+-- this function exists to support clients that pass season order as a number
+DROP FUNCTION IF EXISTS qwerty.getSeason(NUMERIC(1,0));
+
+CREATE FUNCTION qwerty.getSeason(seasonOrder NUMERIC(1,0))
+RETURNS TABLE
+(
+   "Order" NUMERIC(1,0),
+   Name VARCHAR(20),
+   Code CHAR(1)
+)
+AS
+$$
+
+   SELECT "Order", Name, Code
+   FROM qwerty.Season
+   WHERE "Order" = $1;
+
+$$ LANGUAGE sql
+   STABLE
+   RETURNS NULL ON NULL INPUT
+   ROWS 1;
+
+
+
+--Function to get the "order" of the season matching a "season identification"
+DROP FUNCTION IF EXISTS qwerty.getSeasonOrder(VARCHAR(20));
+
+CREATE FUNCTION qwerty.getSeasonOrder(seasonIdentification VARCHAR(20))
+RETURNS NUMERIC(1,0)
+AS
+$$
+
+   SELECT "Order"
+   FROM qwerty.getSeason($1);
+
+$$ LANGUAGE sql
+   STABLE
+   RETURNS NULL ON NULL INPUT;
+
+-- \o
---
+++ qwerty/src/db/addSectionMgmt.sql
@@ -0,0 +1,168 @@
+-- addSectionMgmt.sql
+
+-- Brian Bacon, Jake Homberg
+-- Team Qwerty
+
+-- This script creates functions relating to section management
+-- The functions return information on sections of courses offered
+
+\o spooladdSectionMgmt.txt
+
+\qecho -n 'Script run on '
+\qecho -n `date /t`
+\qecho -n 'at '
+\qecho -n `time /t`
+\qecho -n ' by '
+\qecho :USER
+\qecho ' '
+
+--Suppress messages below WARNING level for the duration of this script
+SET LOCAL client_min_messages TO WARNING;
+
+
+
+
+--Function to get ID of section matching a year-season-course-section# combo
+-- season is "season identification"
+DROP FUNCTION IF EXISTS qwerty.getSectionID(NUMERIC(4,0), VARCHAR(20),
+                                               VARCHAR(8), VARCHAR(3)
+                                              );
+
+CREATE FUNCTION qwerty.getSectionID(year NUMERIC(4,0),
+                                       seasonIdentification VARCHAR(20),
+                                       course VARCHAR(8),
+                                       sectionNumber VARCHAR(3)
+                                      )
+RETURNS INT
+AS
+$$
+
+   SELECT N.ID
+   FROM qwerty.Section N JOIN qwerty.Term T ON N.Term  = T.ID
+   WHERE T.Year = $1
+         AND T.Season = qwerty.getSeasonOrder($2)
+         AND LOWER(N.Course) = LOWER($3)
+         AND LOWER(N.SectionNumber) = LOWER($4);
+
+$$ LANGUAGE sql
+   STABLE
+   RETURNS NULL ON NULL INPUT;
+
+
+--Function to get ID of section matching a year-season-course-section# combo
+-- season is "season order"
+-- reuses the season-identification version
+-- this function exists to support clients that pass season order as a number
+DROP FUNCTION IF EXISTS qwerty.getSectionID(NUMERIC(4,0), NUMERIC(1,0),
+                                               VARCHAR(8), VARCHAR(3)
+                                              );
+
+CREATE FUNCTION qwerty.getSectionID(year NUMERIC(4,0),
+                                       seasonOrder NUMERIC(1,0),
+                                       course VARCHAR(8),
+                                       sectionNumber VARCHAR(3)
+                                      )
+RETURNS INT
+AS
+$$
+
+    SELECT qwerty.getSectionID($1, $2::VARCHAR, $3, $4);
+
+$$ LANGUAGE sql
+ STABLE
+ RETURNS NULL ON NULL INPUT;
+
+
+--Function to get details of section matching a year-season-course-section# combo
+-- input season is "season identification"
+-- StartDate column contains term start date if section does not have start date;
+-- likewise with EndDate column
+DROP FUNCTION IF EXISTS qwerty.getSection(NUMERIC(4,0), VARCHAR(20),
+                                             VARCHAR(8), VARCHAR(3)
+                                            );
+
+CREATE FUNCTION qwerty.getSection(year NUMERIC(4,0),
+                                     seasonIdentification VARCHAR(20),
+                                     course VARCHAR(8), sectionNumber VARCHAR(3)
+                                    )
+RETURNS TABLE
+(
+   ID INT,
+   Term INT,
+   Course VARCHAR(8),
+   SectionNumber VARCHAR(3),
+   CRN VARCHAR(5),
+   Day VARCHAR(2),
+   TimeStart TIME,
+   TimeEnd TIME,
+   Location VARCHAR(25),
+   StartDate DATE,
+   EndDate DATE,
+   MidtermDate DATE,
+   Instructor1 INT,
+   Instructor2 INT,
+   Instructor3 INT
+)
+AS
+$$
+
+   SELECT N.ID, N.Term, N.Course, N.SectionNumber, N.CRN, S.Day,
+          S.TimeStart, S.TimeEnd, N.Location, COALESCE(N.StartDate, T.StartDate),
+          COALESCE(N.EndDate, T.EndDate),
+          N.MidtermDate, N.Instructor1, N.Instructor2, N.Instructor3
+   FROM qwerty.Term T JOIN qwerty.section N ON N.Term  = T.ID
+        JOIN qwerty.Section_Meeting S ON S.SectionID = N.ID
+   WHERE T.Year = $1
+         AND T.Season = qwerty.getSeasonOrder($2)
+         AND LOWER(N.Course) = LOWER($3)
+         AND LOWER(N.SectionNumber) = LOWER($4);
+
+$$ LANGUAGE sql
+   STABLE
+   RETURNS NULL ON NULL INPUT
+   ROWS 1;
+
+
+--Function to get details of section matching a year-season-course-section# combo
+-- input season is "season order"
+-- reuses the season-identification version
+-- this function exists to support clients that pass season order as a number
+DROP FUNCTION IF EXISTS qwerty.getSection(NUMERIC(4,0), NUMERIC(1,0),
+                                             VARCHAR(8), VARCHAR(3)
+                                            );
+
+CREATE FUNCTION qwerty.getSection(year NUMERIC(4,0), seasonOrder NUMERIC(1,0),
+                                    course VARCHAR(8), sectionNumber VARCHAR(3)
+                                   )
+RETURNS TABLE
+(
+     ID INT,
+     Term INT,
+     Course VARCHAR(8),
+     SectionNumber VARCHAR(3),
+     CRN VARCHAR(5),
+     Day VARCHAR(2),
+     TimeStart TIME,
+     TimeEnd TIME,
+     Location VARCHAR(25),
+     StartDate DATE,
+     EndDate DATE,
+     MidtermDate DATE,
+     Instructor1 INT,
+     Instructor2 INT,
+     Instructor3 INT
+)
+AS
+$$
+
+   SELECT ID, Term, Course, SectionNumber, CRN, Day, TimeStart, TimeEnd,
+          Location, StartDate, EndDate,
+          MidtermDate, Instructor1, Instructor2, Instructor3
+   FROM qwerty.getSection($1, $2::VARCHAR, $3, $4);
+
+$$ LANGUAGE sql
+  STABLE
+  RETURNS NULL ON NULL INPUT
+  ROWS 1;
+
+\o
