9e819bd70de0d197fc70d41a51365a8bc0ea7163
Brian Bacon <bacon011@connect.wcsu.edu>
2018-11-08 10:14:22 -0500

    More qwerty stuff

---
+++ qwerty/docs/headerTemplate.sql
@@ -0,0 +1,21 @@
+-- filename.sql
+
+-- Brian Bacon, Jake Homberg
+-- Team Qwerty
+
+-- Short Description of what script file does/why it is needed
+
+-- change spool name to appropriate name if needed and remove this comment
+--\o spoolTestData.txt
+
+--\qecho -n 'Script run on '
+--\qecho -n `date /t`
+--\qecho -n 'at '
+--\qecho -n `time /t`
+--\qecho -n ' by '
+--\qecho :USER
+--\qecho ' '
+
+
+
+-- \o
